91b90ff2f84a1c8f86a3573005fdee11886ff4d5
mealton <titov_yw@mail.ru>
2023-04-11 11:25:42 +0300

    Lab6

--- lab5/blog/articles/views.py
+++ lab5/blog/articles/views.py
@@ -1,10 +1,66 @@
 from .models import Article
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+
+def auth(request):
+    if request.user.is_anonymous:
+        if request.method == "POST":
+            form = {'username': request.POST["username"], 'password': request.POST["password"]}
+
+            if '' in form.values():
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'auth.html', {'form': form})
+
+            user = authenticate(username=request.POST["username"], password=request.POST["password"])
+
+            if user:
+                login(request, user)
+                return redirect('/')
+            else:
+                form['errors'] = u"Извините, пользователь не найден..."
+                return render(request, 'auth.html', {'form': form})
+        else:
+            return render(request, 'auth.html', {})
+    else:
+        return render(request, 'forbidden.html', {})
+
+
+def registration(request):
+    if request.user.is_anonymous:
+        if request.method == "POST":
+
+            form = {
+                'username': request.POST["username"],
+                'email': request.POST["email"],
+                'password': request.POST["password"],
+                'password_confirm': request.POST["password_confirm"],
+            }
+
+            if '' in form.values():
+                form['errors'] = u"Не все поля заполнены"
+                return render(request, 'registration.html', {'form': form})
+            elif form['password'] != form['password_confirm']:
+                form['errors'] = u"Пароли не совпадают"
+                return render(request, 'registration.html', {'form': form})
+
+            User.objects.create_user(form['username'], form['email'], form['password'])
+            return redirect('/')
+        else:
+            return render(request, 'registration.html', {})
+    else:
+        return render(request, 'forbidden.html', {})
 
 
 def archive(request):
-    return render(request, 'archive.html', {"posts": Article.objects.all().order_by('-created_date')})
+    return render(request, 'archive.html',
+                  {
+                      "posts": Article.objects.all().order_by('-created_date'),
+                      "user": request.user
+                  })
 
 
 def create_post(request):
@@ -16,7 +72,6 @@ def create_post(request):
             if Article.objects.filter(title=form["title"]).first():
                 form['errors'] = u"Название новости не уникально..."
                 return render(request, 'create_post.html', {'form': form})
-
 
             if form["text"] and form["title"]:
                 result = Article.objects.create(text=form["text"], title=form["title"], author=request.user)
--- lab5/blog/blog/settings.py
+++ lab5/blog/blog/settings.py
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+REGISTRATION_REDIRECT_URL = '/'
